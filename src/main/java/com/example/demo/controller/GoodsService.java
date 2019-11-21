0382171426ffb4ccb621dec6e33206db47e0c239
muluoyanran <muluoyanran@users.noreply.github.com>
2019-11-21 17:39:05 +0800

    share

--- src/main/java/com/example/demo/controller/GoodsService.java
+++ src/main/java/com/example/demo/controller/GoodsService.java
@@ -19,12 +19,12 @@ public class GoodsService {
     /*
      * 获得商品
      */
-    @GetMapping("products/{productId}")
+    @GetMapping("products/{productId}-in")
     @ApiOperation("获得特定规格商品(内部接口, 给 OrderSevice , UserService 访问)")
     public Object getProduct(@ApiParam(name="productId",value="特定规格商品ID",required=true)
                                  @PathVariable("productId") String productId){ return new Object();}
 
-    @GetMapping("goods/{goodsId}")
+    @GetMapping("goods/{goodsId}-in")
     @ApiOperation("获得商品(内部接口, 给 TopicSevice , FootprintService 访问)")
     public Object getGoods(@ApiParam(name="goodsId",value="商品ID",required=true)
                              @PathVariable("goodsId") String goodsId){ return new Object();}

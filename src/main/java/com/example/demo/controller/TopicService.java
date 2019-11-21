12e21554e14207b6b739b10a9691c01aff87d4fc
Tens <tens444@163.com>
2019-11-21 16:54:44 +0800

    modify

--- src/main/java/com/example/demo/controller/TopicService.java
+++ src/main/java/com/example/demo/controller/TopicService.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+@Api(tags = "专题微服务")
+@RestController
 public class TopicService {
 
     /*

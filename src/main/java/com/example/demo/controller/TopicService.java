e8e8d853f2f107a5c302bed3548b4b8a3417011b
muluoyanran <muluoyanran@users.noreply.github.com>
2019-11-21 17:27:06 +0800

    share

--- src/main/java/com/example/demo/controller/TopicService.java
+++ src/main/java/com/example/demo/controller/TopicService.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -25,6 +23,27 @@ public class TopicService {
     public Object getOneTopic(@ApiParam(name="id",value="话题ID",required=true) @PathVariable("id") String id){ return new Object(); }
 
 
+
+
+    /*
+     * 话题操作
+     */
+
+    @PostMapping("topics/search")
+    @ApiOperation("搜索专题")
+    public ArrayList<Object> searchTopic(@ApiParam(name="name",value="专题标题")   String name){ return new ArrayList<>();}
+
+    @PostMapping("topics")
+    @ApiOperation("添加专题")
+    public Object addTopic(@ApiParam(name="topic",value="专题信息")   Object topic){return new Object(); }
+
+    @DeleteMapping("topic/{id}")
+    @ApiOperation("删除专题")
+    public boolean deleteTopic(@ApiParam(name="id",value="专题ID")  @PathVariable("id") String id){return true; }
+
+    @PatchMapping("topic/{id}")
+    @ApiOperation("修改专题信息")
+    public Object updateTopic(@ApiParam(name="id",value="专题ID")  @PathVariable("id") String id,@ApiParam(name="topic",value="专题信息")   Object topic){ return new Object();}
 
 
 

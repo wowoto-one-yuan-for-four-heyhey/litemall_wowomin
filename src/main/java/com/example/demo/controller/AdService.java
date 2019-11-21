8a40fc1a8dbb12329450357c291e3c48b0ab1b5b
fringe-k <1151712307@qq.com>
2019-11-21 10:00:32 +0800

    5

--- src/main/java/com/example/demo/controller/AdService.java
+++ src/main/java/com/example/demo/controller/AdService.java
@@ -1,4 +1,44 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 public class AdService {
+
+    /*
+     * 广告操作
+     * */
+    @GetMapping("ads/{id}")
+    @ApiOperation("获取特定广告信息")
+    public ArrayList<Object> getAdsById(@ApiParam(name="id",value="广告ID",required=true) @PathVariable("id") String id)
+    { return new ArrayList<>(); }
+
+    @GetMapping("ads")
+    @ApiOperation("查看所有广告信息(管理员)")
+    public ArrayList<Object> getAllAds(){ return new ArrayList<>(); }
+
+    @GetMapping("ads/search")
+    @ApiOperation("搜索广告信息(管理员)")
+    public ArrayList<Object> searchAds(@ApiParam(name="title",value="广告标题")String title,
+                                       @ApiParam(name="content",value="广告内容")String content)
+    { return new ArrayList<>(); }
+
+    @PostMapping("ads")
+    @ApiOperation("添加广告信息(管理员)")
+    public Object createAds(@ApiParam(name="ad",value="广告信息")Object ad){ return new Object(); }
+
+    @DeleteMapping("ads/{id}")
+    @ApiOperation("删除广告信息(管理员)")
+    public boolean deleteAds(@ApiParam(name="id",value="广告ID",required=true)  @PathVariable("id") String id)
+    { return true; }
+
+    @PutMapping("ads/{id}")
+    @ApiOperation("修改广告信息(管理员)")
+    public Object updateAds(@ApiParam(name="id",value="广告ID",required=true)  @PathVariable("id") String id,
+                            @ApiParam(name="ad",value="广告信息")Object ad)
+    {return new Object(); }
+
 }
--- src/main/java/com/example/demo/controller/CommentService.java
+++ src/main/java/com/example/demo/controller/CommentService.java
@@ -1,4 +1,67 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 public class CommentService {
+
+    /*
+     * 用户评论
+     */
+
+    @PostMapping("goods/{id}/comments")
+    @ApiOperation("添加特定商品评论")
+    public Object addComment(@ApiParam(name="id",value="商品id",required=true)@PathVariable("id")String id){return new Object();}
+
+    @GetMapping("goods/{id}/comments")
+    @ApiOperation("查看特定商品评论")
+    public ArrayList<Object> getComment(@ApiParam(name="id",value="商品id",required=true)@PathVariable("id")String id){return new ArrayList<Object>();}
+
+    @GetMapping("topics/{id}/comments")
+    @ApiOperation("查看特定专题的评论")
+    public ArrayList<Object> getCommentsByTopicId(@ApiParam(name="id",value="话题id",required=true)@PathVariable("id")String id){ return new ArrayList<Object>(); }
+
+    @PostMapping("topics/{id}/comments")
+    @ApiOperation("添加特定专题的评论")
+    public Object addTopicComment(@ApiParam(name="id",value="话题id",required=true)@PathVariable("id")String id){ return new Object(); }
+
+    @DeleteMapping("topics/{topicId}/comments/{commentId}")
+    @ApiOperation("删除该用户对特定专题的特定评论")
+    public boolean deleteOneComment(@ApiParam(name="topicId",value="话题id",required=true)@PathVariable("topicId")String topicId,
+                                    @ApiParam(name="commentId",value="话题id",required=true)@PathVariable("commentId")String commentId){ return true; }
+
+    @PutMapping("topics/{topicId}/comments/{commentId}")
+    @ApiOperation("修改该用户对特定专题的特定评论")
+    public Object updateComment(@ApiParam(name="topicId",value="话题id",required=true)@PathVariable("topicId")String topicId,
+                                @ApiParam(name="commentId",value="话题id",required=true)@PathVariable("commentId")String commentId){ return new Object();}
+
+     /*
+      *管理员评论
+      */
+     @GetMapping("goods/comments")
+     @ApiOperation(value = "查看所有商品评论(管理员)")
+     public ArrayList<Object> getAllGoodsComments(){return new ArrayList<>();}
+
+    @GetMapping("goods/comments/search")
+    @ApiOperation(value = "搜索商品评论(管理员)")
+    public ArrayList<Object> searchGoodsComments(@ApiParam(name="userId",value="用户ID")  String userId,
+                                                 @ApiParam(name="goodsId",value="商品ID")  String goodsId)
+    {return new ArrayList<>();}
+
+    @PostMapping("goods/comments/{id}")
+    @ApiOperation(value = "回复商品评论(管理员)")
+    public Object replyGoodsComments(@ApiParam(name="commentId",value="评论ID",required=true) @PathVariable("id") String id,
+                                     @ApiParam(name="reply",value="回复内容")String reply)
+    { return new Object();}
+
+    @DeleteMapping("goods/comments/{id}")
+    @ApiOperation(value = "删除商品评论(管理员)")
+    public boolean deleteGoodsComments(@ApiParam(name="id",value="评论ID",required=true) @PathVariable("id")  String id){return true; }
+
+    @PutMapping("goods/comments/{id}")
+    @ApiOperation(value = "审核商品评论(管理员)")
+    public boolean verifyGoodsComments(@ApiParam(name="id",value="评论ID",required=true) @PathVariable("id")  String id){return true; }
 }
